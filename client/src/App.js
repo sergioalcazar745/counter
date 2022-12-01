975bbd0dd6d15fe033d5f9e22a3de0e54597ce0f
sergioalcazar745 <sergioalcazar745@gmail.com>
2022-12-01 19:51:48 +0100

    por fin

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import io from "socket.io-client";
+import $ from 'jquery';
+import Popper from 'popper.js';
 import { useState, useEffect, useRef } from 'react'
 
 const socket = io('http://localhost:4000/')
@@ -14,24 +16,35 @@ function App() {
   const comenzar = (e) => {
     e.preventDefault();
     socket.emit('contador', numero.current.value);
+    $("#btnEnviar").hide();
+    $("#btnPausar").show();
+    $("#texto").val("");
   }
 
   const pausar = (e) => {
     e.preventDefault();
     socket.emit('contador', "pausa");
+    $("#btnPausar").hide();
+    $("#btnReanudar").show();
   }
 
   const reanudar = (e) => {
     e.preventDefault();
     socket.emit('contador', segundos);
+    $("#btnPausar").show();
+    $("#btnReanudar").hide();
   }
 
   const timer = (numero) => {
     const convertedTime = secConverter(numero, 'sec')
     setClock(convertedTime)
+    $("#clock").show();
   }
 
   useEffect (() => {
+    $("#btnPausar").hide();
+    $("#btnReanudar").hide();
+    $("#clock").hide();
     const receiveMessage = (segundos) => {
       setSegundos(segundos);
       timer(segundos);
@@ -49,13 +62,13 @@ function App() {
     <div className="App">
 
       <form onSubmit={e => comenzar(e)}>
-        <input type="text" ref={numero}/>
-        <button>Send</button>
-        <button type='button' onClick={e => pausar(e)}>Pausar</button>
-        <button type='button' onClick={e => reanudar(e)}>Reanudar</button>
+        <input id='texto' type="text" ref={numero}/>
+        <button id='btnEnviar'>Send</button>
+        <button type='button' id='btnPausar' onClick={e => pausar(e)}>Pausar</button>
+        <button type='button' id='btnReanudar' onClick={e => reanudar(e)}>Reanudar</button>
       </form>
 
-      <h1>Reloj: {clock.hours}:{clock.minutes}:{clock.seconds}</h1>
+      <h1 id='clock'>Reloj: {clock.hours}:{clock.minutes}:{clock.seconds}</h1>
 
     </div>
   );
