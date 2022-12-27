06bf5494905412de2f1846151752a89d61bb2f66
sergioalcazar745 <sergioalcazar745@gmail.com>
2022-12-27 01:47:31 +0100

    lets go

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,6 @@
 import React, { Component } from 'react';
 import './../assests/css/Home.css';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import $ from 'jquery';
 import io from "socket.io-client";
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assests/css/index.css';
 import App from './components/App';
-import Router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <Router />
+    <App />
   // </React.StrictMode>
 );
