f7eb7ceaa8a873886a0e5b367ad486d882a02fcd
juancarlosmaranon <juancar2001jcmg@gmail.com>
2022-12-29 13:53:48 +0100

    A

--- client/src/services/SalaService.js
+++ client/src/services/SalaService.js
@@ -4,7 +4,13 @@ import axios from "axios";
 export default class EmpresaService{
 
     getSalas(){
-        
+        return new Promise(function(resolve) {
+            var request = Global.url + "api/empresas";
+
+            axios.get(request).then(response => {
+                resolve(response.data)
+            });
+        });
     }
 
 }
